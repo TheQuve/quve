161b1f2f1c7e3481e9c21e631b3b989d0687cddb
parkyes90 <parkyes90@gmail.com>
2019-01-13 22:17:03 +0900

    bug fix

--- user/models.py
+++ user/models.py
@@ -20,7 +20,7 @@ class User(AbstractUser, TimeStampedModel):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    username = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     website = models.URLField(null=True)
     bio = models.TextField(null=True)
