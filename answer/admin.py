dbe52fa3cba001cae9c66e4e77981fb0727928a3
parkyes90 <parkyes90@gmail.com>
2019-01-19 12:42:43 +0900

    admin 생성

--- answer/admin.py
+++ answer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from answer.models import Answer
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Answer._meta.fields]
--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from question.models import Question
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Question._meta.fields]
